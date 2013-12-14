50e29515c5734eb48072c77e1accbaa12f9d23e2
Lilleman <lilleman@larvit.se>
2013-12-14 16:35:45 +0100

    Fixed compability with the new session system

--- classes/controller/admin/login.php
+++ classes/controller/admin/login.php
@@ -1,30 +1,22 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class Controller_Admin_Login extends Admincontroller {
+class Controller_Admin_Login extends Admincontroller
+{
 
 	public function action_index()
 	{
-		Session::instance();
 		$this->xslt_stylesheet = 'admin/login';
 		$this->ignore_acl      = TRUE; // This page should be accessible by everyone
 
 		$user = User::instance();
 		if ($user->has_access_to(URL::base().'admin'))
 			$this->redirect(URL::base().'admin');
-
-		if (isset($_SESSION['modules']['pajas']['error']))
-		{
-			xml::to_XML(array('error' => $_SESSION['modules']['pajas']['error']), $this->xml_content);
-			unset($_SESSION['modules']['pajas']['error']);
-		}
 	}
 
 	public function action_do()
 	{
 		if (count($_POST) && isset($_POST['username']) && isset($_POST['password']))
 		{
-			Session::instance();
-
 			$post = new Validation($_POST);
 			$post->filter('trim');
 			$post->filter('strtolower', 'username'); // Usename should always be lower case
@@ -35,11 +27,11 @@ class Controller_Admin_Login extends Admincontroller {
 			if ($user->logged_in() && $user->has_access_to(URL::base().'admin'))
     		$this->redirect('admin');
 			elseif ( ! $user->logged_in())
-				$_SESSION['modules']['pajas']['error'] = 'Wrong username or password';
+				$this->add_error('Wrong username or password', FALSE, TRUE);
 			elseif ( ! $user->has_access_to(URL::base().'admin'))
-				$_SESSION['modules']['pajas']['error'] = 'You are not authorized';
+				$this->add_error('You are not authorized', FALSE, TRUE);
 			else
-				$_SESSION['modules']['pajas']['error'] = 'Unknown error';
+				$this->add_error('Unknown error', FALSE, TRUE);
 		}
 		$this->redirect();
 	}
